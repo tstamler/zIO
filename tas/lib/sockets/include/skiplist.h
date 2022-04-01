e5fc137e684c9819727cdd0af0e77f4c9e19b62d
deukyeon <deukyeon.hwang@gmail.com>
2022-04-01 19:02:47 +0000

    backup

--- tas/lib/sockets/include/skiplist.h
+++ tas/lib/sockets/include/skiplist.h
@@ -3,15 +3,17 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #define SKIPLIST_MAX_LEVEL 6
 
+// address
 typedef struct snode {
-  uint64_t lookup;  // lookup key
-  uint64_t orig;    // the original buffer address
-  uint64_t offset;  // left fringe size
-  uint64_t core;    // core buffer address
-  uint32_t len;     // core buffer size
+  uint64_t lookup; // lookup key
+  uint64_t orig;   // the original buffer address
+  uint64_t offset; // left fringe size
+  uint64_t addr;   // buffer address
+  uint64_t len;
   uint8_t free;
   struct snode **forward;
 } snode;
@@ -41,27 +43,38 @@ static inline skiplist *skiplist_init(skiplist *list) {
 
 static inline int rand_level() {
   int level = 1;
-  while (rand() < RAND_MAX / 2 && level < SKIPLIST_MAX_LEVEL) level++;
+  while (rand() < RAND_MAX / 2 && level < SKIPLIST_MAX_LEVEL)
+    level++;
   return level;
 }
 
-static inline int skiplist_insert(skiplist *list, uint64_t lookup,
-                                  uint64_t orig, uint64_t core, uint32_t len,
-                                  uint64_t offset) {
+static inline int skiplist_insert_with_addr(skiplist *list, uint64_t lookup,
+                                            uint64_t orig, uint64_t addr,
+                                            uint32_t len, uint64_t offset) {
   snode *update[SKIPLIST_MAX_LEVEL + 1];
   snode *x = list->header;
   int i, level;
   for (i = list->level; i >= 1; i--) {
-    while (x->forward[i] && x->forward[i]->lookup < lookup) x = x->forward[i];
+    while (x->forward[i] && x->forward[i]->lookup < lookup)
+      x = x->forward[i];
     update[i] = x;
   }
+
+  // snode *prev = x->forward[1];
+  // if (prev->addr + prev->offset + prev->len + getpagesize() >= addr) {
+  //   // If a new buffer is consecutive with the existing one
+  //   // merge to the previous node
+  //   prev->len += len + getpagesize();
+  //   return 0;
+  // }
+
   x = x->forward[1];
 
   if (x && lookup == x->lookup) {
     x->orig = orig;
     x->len = len;
     x->offset = offset;
-    x->core = core;
+    x->addr = addr;
     return 0;
   } else {
     level = rand_level();
@@ -75,7 +88,7 @@ static inline int skiplist_insert(skiplist *list, uint64_t lookup,
     x = (snode *)malloc(sizeof(snode));
     x->lookup = lookup;
     x->orig = orig;
-    x->core = core;
+    x->addr = addr;
     x->len = len;
     x->offset = offset;
     x->forward = (snode **)malloc(sizeof(snode *) * (level + 1));
@@ -87,16 +100,23 @@ static inline int skiplist_insert(skiplist *list, uint64_t lookup,
   return 0;
 }
 
+static inline int skiplist_insert(skiplist *list, uint64_t lookup,
+                                  uint64_t orig, uint32_t len,
+                                  uint64_t offset) {
+  return skiplist_insert_with_addr(list, lookup, orig, lookup, len, offset);
+}
+
 static inline int skiplist_insert_entry(skiplist *list, snode *entry) {
-  return skiplist_insert(list, entry->lookup, entry->orig, entry->core,
-                         entry->len, entry->offset);
+  return skiplist_insert_with_addr(list, entry->lookup, entry->orig,
+                                   entry->addr, entry->len, entry->offset);
 }
 
 static inline snode *skiplist_search(skiplist *list, uint64_t lookup) {
   snode *x = list->header;
   int i;
   for (i = list->level; i >= 1; i--) {
-    while (x->forward[i]->lookup < lookup) x = x->forward[i];
+    while (x->forward[i]->lookup < lookup)
+      x = x->forward[i];
   }
   if (x->forward[1]->lookup == lookup) {
     return x->forward[1];
@@ -110,9 +130,9 @@ static inline snode *skiplist_search(skiplist *list, uint64_t lookup) {
 static inline snode *skiplist_search_buffer_fallin(skiplist *list,
                                                    uint64_t addr) {
   snode *x = list->header;
-  while (x->forward[1]) {
-    if (x->forward[1]->core <= addr &&
-        addr < x->forward[1]->core + x->forward[1]->len)
+  while (x && x->forward[1] != list->header) {
+    if (x->forward[1]->addr + x->forward[1]->offset <= addr &&
+        addr < x->forward[1]->addr + x->forward[1]->offset + x->forward[1]->len)
       return x->forward[1];
     x = x->forward[1];
   }
@@ -122,6 +142,19 @@ static inline snode *skiplist_search_buffer_fallin(skiplist *list,
 
 static inline void skiplist_node_free(snode *x) {
   if (x) {
+
+    {
+      fprintf(stderr, "\tlookup: %p\n", (void *)x->lookup);
+      fprintf(stderr, "\torig: %p\n", (void *)x->orig);
+      fprintf(stderr, "\taddr: %p\n", (void *)x->addr);
+      fprintf(stderr, "\tlen: %lu\n", x->len);
+      fprintf(stderr, "\toffset: %lu\n", x->offset);
+      fprintf(stderr, "\tcore_buffer: %p-%p\n", x->addr + x->offset,
+              x->addr + x->offset + x->len);
+      fprintf(stderr, "\tcorresponding original: %p-%p\n", x->orig + x->offset,
+              x->orig + x->offset + x->len);
+    }
+
     free(x->forward);
     free(x);
   }
@@ -132,14 +165,16 @@ static inline int skiplist_delete(skiplist *list, uint64_t lookup) {
   snode *update[SKIPLIST_MAX_LEVEL + 1];
   snode *x = list->header;
   for (i = list->level; i >= 1; i--) {
-    while (x->forward[i]->lookup < lookup) x = x->forward[i];
+    while (x->forward[i] && x->forward[i]->lookup < lookup)
+      x = x->forward[i];
     update[i] = x;
   }
 
   x = x->forward[1];
-  if (x->lookup == lookup) {
+  if (x && x->lookup == lookup) {
     for (i = 1; i <= list->level; i++) {
-      if (update[i]->forward[i] != x) break;
+      if (update[i]->forward[i] != x)
+        break;
       update[i]->forward[i] = x->forward[i];
     }
     skiplist_node_free(x);
@@ -152,12 +187,35 @@ static inline int skiplist_delete(skiplist *list, uint64_t lookup) {
   return 1;
 }
 
+static inline snode *skiplist_front(skiplist *list) {
+  snode *x = list->header;
+  return x->forward[1];
+}
+
 static inline void skiplist_dump(skiplist *list) {
   snode *x = list->header;
   while (x && x->forward[1] != list->header) {
-    printf("%lu[%lu, %lu, %u]->", x->forward[1]->lookup, x->forward[1]->orig,
-           x->forward[1]->core, x->forward[1]->len);
+    printf("%p[%p, %p, %u, %u]->", x->forward[1]->lookup,
+           (void *)x->forward[1]->orig, (void *)x->forward[1]->addr,
+           x->forward[1]->len, x->forward[1]->offset);
     x = x->forward[1];
   }
   printf("NIL\n");
+}
+
+static inline snode *snode_get_next(skiplist *list, snode *node) {
+  return node && node->forward[1] != list->header ? node->forward[1] : NULL;
+}
+
+static inline void snode_dump(const snode *node) {
+  //   fprintf(stdout, "snode %p\n", node);
+  fprintf(stderr, "\tlookup: %p\n", (void *)node->lookup);
+  fprintf(stderr, "\torig: %p\n", (void *)node->orig);
+  fprintf(stderr, "\taddr: %p\n", (void *)node->addr);
+  fprintf(stderr, "\tlen: %lu\n", node->len);
+  fprintf(stderr, "\toffset: %lu\n", node->offset);
+  fprintf(stderr, "\tcore_buffer: %p-%p\n", node->addr + node->offset,
+          node->addr + node->offset + node->len);
+  fprintf(stderr, "\tcorresponding original: %p-%p\n",
+          node->orig + node->offset, node->orig + node->offset + node->len);
 }
